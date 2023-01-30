5632a1515c96ae76349d923ad8bbb44a49446ea4
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2023-01-30 17:21:15 +0000

    changing sampling params

--- adni/inference/local-fkpp/run-inference.jl
+++ adni/inference/local-fkpp/run-inference.jl
@@ -9,5 +9,5 @@ addprocs(4)
 file_path = projectdir("adni/inference/local-fkpp/taupos.jl")
 @everywhere include($(file_path))
 
-pst = sample(m, Turing.NUTS(0.8), MCMCDistributed(), 1000, 4)
+pst = sample(m, Turing.NUTS(0.8), MCMCDistributed(), 500, 2)
 serialize(projectdir("adni/chains/local-fkpp/pst-taupos-distributed.jls"), pst)
