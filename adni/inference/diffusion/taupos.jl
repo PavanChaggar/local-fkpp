dffee96df72d93faf94beb12873800cad0dfd9b9
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-01-30 19:55:44 +0000

    adding diffusion scripts

---
+++ adni/inference/diffusion/taupos.jl
@@ -0,0 +1,167 @@
+using Pkg
+cd("/home/chaggar/Projects/local-fkpp")
+Pkg.activate(".")
+
+using Connectomes
+using ADNIDatasets
+using CSV, DataFrames
+using DrWatson: projectdir
+using DifferentialEquations
+using SciMLSensitivity
+using Zygote
+using Turing
+using AdvancedHMC
+using Distributions
+using Serialization
+using DelimitedFiles, LinearAlgebra
+using Random
+using LinearAlgebra
+using SparseArrays
+include(projectdir("functions.jl"))
+#-------------------------------------------------------------------------------
+# Connectome and ROIs
+#-------------------------------------------------------------------------------
+connectome_path = Connectomes.connectome_path()
+all_c = filter(Connectome(connectome_path; norm=true), 1e-2);
+
+subcortex = filter(x -> x.Lobe == "subcortex", all_c.parc);
+cortex = filter(x -> x.Lobe != "subcortex", all_c.parc);
+
+c = slice(all_c, cortex) |> filter
+
+mtl_regions = ["entorhinal", "Left-Amygdala", "Right-Amygdala"]
+mtl = findall(x -> x ∈ mtl_regions, cortex.Label)
+neo_regions = ["inferiortemporal", "middletemporal"]
+neo = findall(x -> x ∈ neo_regions, cortex.Label)
+#-------------------------------------------------------------------------------
+# Data 
+#-----------------------------------------------------------------------------
+sub_data_path = projectdir("adni/data/AV1451_Diagnosis-STATUS-STIME-braak-regions.csv")
+alldf = CSV.read(sub_data_path, DataFrame)
+
+posdf = filter(x -> x.STATUS == "POS", alldf)
+
+dktdict = Connectomes.node2FS()
+dktnames = [dktdict[i] for i in cortex.ID]
+
+data = ADNIDataset(posdf, dktnames; min_scans=3)
+
+# Ask Jake where we got these cutoffs from? 
+mtl_cutoff = 1.375
+neo_cutoff = 1.395
+
+mtl_pos = filter(x -> regional_mean(data, mtl, x) >= mtl_cutoff, 1:50)
+neo_pos = filter(x -> regional_mean(data, neo, x) >= neo_cutoff, 1:50)
+
+tau_pos = findall(x -> x ∈ unique([mtl_pos; neo_pos]), 1:50)
+tau_neg = findall(x -> x ∉ tau_pos, 1:50)
+
+n_pos = length(tau_pos)
+n_neg = length(tau_neg)
+#-------------------------------------------------------------------------------
+# Connectome + ODEE
+#-------------------------------------------------------------------------------
+L = laplacian_matrix(c)
+
+vols = [get_vol(data, i) for i in tau_pos]
+init_vols = [v[:,1] for v in vols]
+max_norm_vols = reduce(hcat, [v ./ maximum(v) for v in init_vols])
+mean_norm_vols = vec(mean(max_norm_vols, dims=2))
+Lv = sparse(inv(diagm(mean_norm_vols)) * L)
+
+function NetworkDiffusion(du, u, p, t; Lv = Lv)
+    du .= -p[1] * Lv * u
+end
+
+function make_prob_func(initial_conditions, p, times)
+    function prob_func(prob,i,repeat)
+        remake(prob, u0=initial_conditions[i], p=p, saveat=times[i])
+    end
+end
+
+function output_func(sol,i)
+    (sol,false)
+end
+
+subdata = [calc_suvr(data, i) for i in tau_pos]
+
+vecsubdata = reduce(vcat, reduce(hcat, subdata))
+
+initial_conditions = [sd[:,1] for sd in subdata]
+times =  [get_times(data, i) for i in tau_pos]
+max_t = maximum(reduce(vcat, times))
+
+prob = ODEProblem(NetworkDiffusion, 
+                  initial_conditions[1], 
+                  (0.,max_t), 
+                  1.0)
+                  
+sol = solve(prob, Tsit5())
+
+ensemble_prob = EnsembleProblem(prob, prob_func=make_prob_func(initial_conditions, ones(n_pos), times), output_func=output_func)
+ensemble_sol = solve(ensemble_prob, Tsit5(), trajectories=n_pos)
+
+@inline function allequal(x)
+    length(x) < 2 && return true
+    e1 = x[1]
+    i = 2
+    @inbounds for i=2:length(x)
+        x[i] == e1 || return false
+    end
+    return true
+end
+
+function get_retcodes(es)
+    [sol.retcode for sol in es]
+end
+
+function vec_sol(es)
+    reduce(vcat, [vec(sol) for sol in es])
+end
+
+#-------------------------------------------------------------------------------
+# Inference 
+#-------------------------------------------------------------------------------
+@model function diffusion(data, prob, initial_conditions, times, n)
+    σ ~ LogNormal(0, 1)
+    
+    Pm ~ LogNormal(0, 0.5)
+    Ps ~ LogNormal(0, 0.5)
+
+    ρ ~ filldist(truncated(Normal(Pm, Ps), lower=0), n)
+
+    ensemble_prob = EnsembleProblem(prob, 
+                                    prob_func=make_prob_func(initial_conditions, ρ, times), 
+                                    output_func=output_func)
+
+    ensemble_sol = solve(ensemble_prob, 
+                         Tsit5(), 
+                         abstol = 1e-9, 
+                         reltol = 1e-9, 
+                         trajectories=n, 
+                         sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))
+
+    if !allequal(get_retcodes(ensemble_sol)) 
+        Turing.@addlogprob! -Inf
+        println("failed")
+        return nothing
+    end
+    vecsol = vec_sol(ensemble_sol)
+
+    data ~ MvNormal(vecsol, σ^2 * I)
+end
+
+setadbackend(:zygote)
+Random.seed!(1234);
+
+m = diffusion(vecsubdata, prob, initial_conditions, times, n_pos);
+m();
+
+n_chains = 4
+pst = sample(m, 
+             Turing.NUTS(0.8), #, metricT=AdvancedHMC.DenseEuclideanMetric), 
+             MCMCSerial(), 
+             2_000, 
+             n_chains,
+             progress=false)
+serialize(projectdir("adni/chains/diffusion/pst-taupos-$(n_chains)x2000-vc.jls"), pst)
