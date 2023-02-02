095b63202aa2e2b5830a00c69216559e10d4af02
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-02-02 12:26:36 +0000

    adding script for individual inference

---
+++ adni/inference/local-fkpp/individual_inference.jl
@@ -0,0 +1,173 @@
+using Pkg
+cd("/home/chaggar/Projects/local-fkpp")
+Pkg.activate(".")
+
+using Connectomes
+using ADNIDatasets
+using CSV, DataFrames
+using DrWatson: projectdir
+using DifferentialEquations
+using SciMLSensitivity
+using Zygote
+using Turing
+using AdvancedHMC
+using Distributions
+using Serialization
+using DelimitedFiles, LinearAlgebra
+using Random
+using LinearAlgebra
+using SparseArrays
+include(projectdir("functions.jl"))
+#-------------------------------------------------------------------------------
+# Connectome and ROIs
+#-------------------------------------------------------------------------------
+connectome_path = Connectomes.connectome_path()
+all_c = filter(Connectome(connectome_path; norm=true), 1e-2);
+
+subcortex = filter(x -> x.Lobe == "subcortex", all_c.parc);
+cortex = filter(x -> x.Lobe != "subcortex", all_c.parc);
+
+c = slice(all_c, cortex) |> filter
+
+mtl_regions = ["entorhinal", "Left-Amygdala", "Right-Amygdala"]
+mtl = findall(x -> x ∈ mtl_regions, cortex.Label)
+neo_regions = ["inferiortemporal", "middletemporal"]
+neo = findall(x -> x ∈ neo_regions, cortex.Label)
+#-------------------------------------------------------------------------------
+# Data 
+#-----------------------------------------------------------------------------
+sub_data_path = projectdir("adni/data/AV1451_Diagnosis-STATUS-STIME-braak-regions.csv")
+alldf = CSV.read(sub_data_path, DataFrame)
+
+posdf = filter(x -> x.STATUS == "POS", alldf)
+
+dktdict = Connectomes.node2FS()
+dktnames = [dktdict[i] for i in cortex.ID]
+
+data = ADNIDataset(posdf, dktnames; min_scans=3)
+
+# # Ask Jake where we got these cutoffs from? 
+# mtl_cutoff = 1.375
+# neo_cutoff = 1.395
+
+# mtl_pos = filter(x -> regional_mean(data, mtl, x) >= mtl_cutoff, 1:50)
+# neo_pos = filter(x -> regional_mean(data, neo, x) >= neo_cutoff, 1:50)
+
+# tau_pos = findall(x -> x ∈ unique([mtl_pos; neo_pos]), 1:50)
+# tau_neg = findall(x -> x ∉ tau_pos, 1:50)
+
+# n_pos = length(tau_pos)
+# n_neg = length(tau_neg)
+n_subs = collect(1:length(data))
+
+gmm_moments = CSV.read(projectdir("adni/data/component_moments.csv"), DataFrame)
+#gmm_moments2 = CSV.read(projectdir("data/adni-data/component_moments-bothcomps.csv"), DataFrame)
+ubase, upath = get_dkt_moments(gmm_moments, dktnames)
+u0 = mean.(ubase)
+cc = quantile.(upath, .99)
+#-------------------------------------------------------------------------------
+# Connectome + ODEE
+#-------------------------------------------------------------------------------
+L = laplacian_matrix(c)
+
+vols = [get_vol(data, i) for i in tau_pos]
+init_vols = [v[:,1] for v in vols]
+max_norm_vols = reduce(hcat, [v ./ maximum(v) for v in init_vols])
+mean_norm_vols = vec(mean(max_norm_vols, dims=2))
+Lv = sparse(inv(diagm(mean_norm_vols)) * L)
+
+function NetworkLocalFKPP(du, u, p, t; Lv = Lv, u0 = u0, cc = cc)
+    du .= -p[1] * Lv * (u .- u0) .+ p[2] .* (u .- u0) .* ((cc .- u0) .- (u .- u0))
+end
+
+function make_prob_func(initial_conditions, p, a, times)
+    function prob_func(prob,i,repeat)
+        remake(prob, u0=initial_conditions[i], p=[p[i], a[i]], saveat=times[i])
+    end
+end
+
+function output_func(sol,i)
+    (sol,false)
+end
+
+subdata = [calc_suvr(data, i) for i in n_subs]
+for i in n_subs
+    normalise!(subdata[i], u0, cc)
+end
+
+initial_conditions = [sd[:,1] for sd in subdata]
+times =  [get_times(data, i) for i in n_subs]
+
+prob = ODEProblem(NetworkLocalFKPP, 
+                  initial_conditions[1], 
+                  (0.,6), 
+                  [1.0,1.0])
+                  
+sol = solve(prob, Tsit5())
+
+ensemble_prob = EnsembleProblem(prob, prob_func=make_prob_func(initial_conditions, ones(n_pos), ones(n_pos), times), output_func=output_func)
+ensemble_sol = solve(ensemble_prob, Tsit5(), trajectories=n_pos)
+
+@inline function allequal(x)
+    length(x) < 2 && return true
+    e1 = x[1]
+    i = 2
+    @inbounds for i=2:length(x)
+        x[i] == e1 || return false
+    end
+    return true
+end
+
+function get_retcodes(es)
+    [sol.retcode for sol in es]
+end
+
+function vec_sol(es)
+    reduce(vcat, [vec(sol) for sol in es])
+end
+
+#-------------------------------------------------------------------------------
+# Inference 
+#-------------------------------------------------------------------------------
+@model function localfkpp(data, prob, initial_conditions, times, subs, n)
+    σ ~ LogNormal(0.0, 1.0)
+    
+    ρ ~ filldist(LogNormal(0.0,1.0), n)
+    α ~ filldist(Normal(0.0, 1.0), n)
+
+    ensemble_prob = EnsembleProblem(prob, 
+                                    prob_func=make_prob_func(initial_conditions, ρ, α, times), 
+                                    output_func=output_func)
+
+    ensemble_sol = solve(ensemble_prob, 
+                         Tsit5(), 
+                         abstol = 1e-9, 
+                         reltol = 1e-9, 
+                         trajectories=n, 
+                         sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))
+    if !allequal(get_retcodes(ensemble_sol)) 
+        Turing.@addlogprob! -Inf
+        println("failed")
+        return nothing
+    end
+
+    for i in subs
+        data[i] ~ MvNormal(vec(ensemble_sol[i]), σ^2 * I)
+    end
+end
+
+setadbackend(:zygote)
+Random.seed!(1234)
+
+vecsubdata = [reduce(vcat, sd) for sd in subdata]
+m = localfkpp(vecsubdata, prob, initial_conditions, times, n_subs, length(n_subs));
+m();
+
+n_chains = 1
+pst = sample(m, 
+             Turing.NUTS(0.8), #, metricT=AdvancedHMC.DenseEuclideanMetric), 
+             MCMCSerial(), 
+             2_000, 
+             n_chains,
+             progress=false)
+serialize(projectdir("adni/chains/local-fkpp/pst-taupos-$(n_chains)x2000-individual.jls"), pst)
