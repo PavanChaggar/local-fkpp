18f0139b6fe7f04a9792d4683171e0fd25eb2088
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-02-10 17:04:03 +0000

    adding diffusion models

--- adni/inference/diffusion/taupos.jl
+++ adni/inference/diffusion/taupos.jl
@@ -110,16 +110,6 @@ sol = solve(prob, Tsit5())
 ensemble_prob = EnsembleProblem(prob, prob_func=make_prob_func(initial_conditions, collect(1:27), times), output_func=output_func)
 ensemble_sol = solve(ensemble_prob, Tsit5(), trajectories=n_pos)
 
-@inline function allequal(x)
-    length(x) < 2 && return true
-    e1 = x[1]
-    i = 2
-    @inbounds for i=2:length(x)
-        x[i] == e1 || return false
-    end
-    return true
-end
-
 function get_retcodes(es)
     [sol.retcode for sol in es]
 end
@@ -134,8 +124,8 @@ end
 @model function diffusion(data, prob, initial_conditions, times, n)
     σ ~ LogNormal(0, 1)
     
-    Pm ~ LogNormal(0, 0.5)
-    Ps ~ LogNormal(0, 0.5)
+    Pm ~ LogNormal(0, 1.0)
+    Ps ~ LogNormal(0, 1.0)
 
     ρ ~ filldist(truncated(Normal(Pm, Ps), lower=0), n)
 
