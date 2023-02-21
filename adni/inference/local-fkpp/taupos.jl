9e2babef7aa8d774da3ad44a16baa12afdbc0a9f
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-02-21 20:37:24 +0000

    testing truncated normal prior

--- adni/inference/local-fkpp/taupos.jl
+++ adni/inference/local-fkpp/taupos.jl
@@ -124,7 +124,7 @@ end
 @model function localfkpp(data, prob, initial_conditions, times, n)
     σ ~ LogNormal(0.0, 1.0)
 
-    Pm ~ LogNormal(0.0, 1.0)
+    Pm ~ truncated(Normal(0.0, 1.0), lower=0)
     Ps ~ LogNormal(0.0, 1.0)
 
     Am ~ Normal(0.0, 1.0)
@@ -161,11 +161,11 @@ m = localfkpp(vecsubdata, prob, initial_conditions, times, n_pos);
 m();
 
 n_chains = 4
-n_samples = 2_000
+n_samples = 1_000
 pst = sample(m, 
              Turing.NUTS(0.8),
              MCMCSerial(), 
-             2_000, 
+             n_samples, 
              n_chains,
              progress=false)
-serialize(projectdir("adni/chains/local-fkpp/pst-taupos-$(n_chains)x$(n_samples)-vc.jls"), pst)
+serialize(projectdir("adni/chains/local-fkpp/pst-taupos-$(n_chains)x$(n_samples)-vc-tn.jls"), pst)
