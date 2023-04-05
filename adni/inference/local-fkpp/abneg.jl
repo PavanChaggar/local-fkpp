96488786fdb0e15126d400552404d95d30a4faa1
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-04-05 09:38:44 +0100

    wrong graph laplacian for abneg

--- adni/inference/local-fkpp/abneg.jl
+++ adni/inference/local-fkpp/abneg.jl
@@ -64,9 +64,6 @@ max_norm_vols = reduce(hcat, [v ./ maximum(v) for v in init_vols])
 mean_norm_vols = vec(mean(max_norm_vols, dims=2))
 Lv = sparse(inv(diagm(mean_norm_vols)) * L)
 
-v = vec(mean(reduce(hcat, init_vols), dims=2))
-Lv = inv(1 / mean(v) * diagm(v)) * L
-
 function NetworkLocalFKPP(du, u, p, t; Lv = Lv, u0 = u0, cc = cc)
     du .= -p[1] * Lv * (u .- u0) .+ p[2] .* (u .- u0) .* ((cc .- u0) .- (u .- u0))
 end
