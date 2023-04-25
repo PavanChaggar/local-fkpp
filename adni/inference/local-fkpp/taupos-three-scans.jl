ad4366fdfa9401987e4a6bbcd00ac8ddfd2855f9
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2023-04-25 14:11:57 +0100

    out of sample three plus update connectome

--- adni/inference/local-fkpp/taupos-three-scans.jl
+++ adni/inference/local-fkpp/taupos-three-scans.jl
@@ -21,7 +21,7 @@ include(projectdir("functions.jl"))
 # Connectome and ROIs
 #-------------------------------------------------------------------------------
 connectome_path = Connectomes.connectome_path()
-all_c = filter(Connectome(connectome_path; norm=true), 1e-2);
+all_c = filter(Connectome(connectome_path; norm=true, weight_function = (n, l) -> n ./ l), 1e-2);
 
 subcortex = filter(x -> x.Lobe == "subcortex", all_c.parc);
 cortex = filter(x -> x.Lobe != "subcortex", all_c.parc);
