57baf654608c39b9266ed4cf0abcc6a6db0a6e2e
PavanChaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2024-03-03 22:32:15 +0000

    updating seeding

--- adni/seeding/seeding.jl
+++ adni/seeding/seeding.jl
@@ -22,42 +22,77 @@ inits[[27]] .= 0.1
 
 prob = ODEProblem(ScaledNetworkLocalFKPP, 
                   inits, 
-                  (0.,10.), 
+                  (0.,15.), 
                   [0.1, 1.0])
                   
 sol = solve(prob, Tsit5(), saveat=[3.0, 4.0, 5.0])
 
 Plots.plot(sol, labels=false)
 
-d = clamp.(vec(sol) .+ (randn(length(vec(sol))) .* 0.025), 0.0, 1.0)
+d = vec(sol) .+ (randn(length(vec(sol))) .* 0.025)
 
 @model function seeding(prob, ts)
     σ ~ InverseGamma(2, 3)
 
     ρ ~ truncated(Normal(), lower=0)
     α ~ truncated(Normal(), lower=0)
+    t ~ Uniform(0, 5)
+    u ~ Dirichlet(36, 0.1)
 
-    u ~ Dirichlet(36, 0.01)
+    _ts = t .+ ts
+    tspan = convert.(eltype(t),(0.0,maximum(_ts)))
 
-    prob = remake(prob, u0 = u .* 0.1, p = [ρ, α])
-                    
-    sol = solve(prob, Tsit5(), abstol=1e-9, reltol=1e-6, saveat=ts)
+    _prob = remake(prob, u0= u .* 0.1, tspan=tspan, p=[ρ, α])
+
+    sol = solve(_prob, Tsit5(), abstol=1e-9, reltol=1e-6, saveat=_ts)
 
     if SciMLBase.successful_retcode(sol) !== true
         Turing.@addlogprob! -Inf
         println("failed")
     end
 
-    d ~ MvNormal(vec(sol), σ^2 * I)
+    data ~ MvNormal(vec(sol), σ^2 * I)
 end
 
-m = seeding(prob, [3.0, 4.0, 5.0])
+m = seeding(prob, [0.0, 1.0, 2.0])
 m()
 
-pst = m | (d = d,)
+pst = m | (data = d,)
 pst()
 
-pst_samples = sample(pst, NUTS(adtype=ADTypes.AutoZygote()), 1_000)
+using TuringBenchmarking, ADTypes
+
+results = TuringBenchmarking.benchmark_model(pst; adbackends=[ADTypes.AutoForwardDiff(chunksize=40)])
+
+pst_samples = sample(pst, NUTS(0.9), 1_000)
 
 meanpst = mean(pst_samples)
 scatter([meanpst["u[$i]", :mean] for i in 1:36] .* 0.2)
+# #------------------------------------------------------------------------
+# # Data Application
+# #------------------------------------------------------------------------
+# data = ADNIDataset(posdf, dktnames; min_scans=4)
+# n_data = length(data)
+# # Ask Jake where we got these cutoffs from? 
+# mtl_cutoff = 1.375
+# neo_cutoff = 1.395
+
+# mtl_pos = filter(x -> regional_mean(data, mtl, x) >= mtl_cutoff, 1:n_data)
+# neo_pos = filter(x -> regional_mean(data, neo, x) >= neo_cutoff, 1:n_data)
+
+# tau_pos = findall(x -> x ∈ mtl_pos && x ∈ neo_pos, 1:n_data)
+
+# taudata = data[tau_pos]
+
+# _subdata = [calc_suvr(data, i) for i in tau_pos]
+# subdata = [normalise(sd, u0, cc) for sd in _subdata]
+
+# t = get_times.(subdata)
+
+# c = [(s[1:36,:] .- u0[1:36]) ./ (cc[1:36] .- u0[1:36]) for s in subdata]
+
+# m = seeding(prob, t[1])
+# m()
+
+# pst = m | (d = vec(c[1]),)
+# pst()
