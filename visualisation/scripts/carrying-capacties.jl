6323f261d9f5e0020f9fcad09cab07841c926e34
Pavan Chaggar <56822454+PavanChaggar@users.noreply.github.com>
2024-05-16 18:10:07 +0100

    bug in carrying capacities

--- visualisation/scripts/carrying-capacties.jl
+++ visualisation/scripts/carrying-capacties.jl
@@ -18,8 +18,31 @@ dktdict = Connectomes.node2FS()
 dktnames = [dktdict[i] for i in get_node_id.(cortex)]
 gmm_moments = CSV.read(projectdir("adni/data/component_moments.csv"), DataFrame)
 norm, path = get_dkt_moments(gmm_moments, dktnames)
-u0 = mean.(norm)
-cc = quantile.(path, .99)
+
+gmm_weights = CSV.read(projectdir("adni/data/component_weights.csv"), DataFrame)
+dktweights= filter(x -> x.Column1 ∈ dktnames, gmm_weights)
+
+function get_dkt_weights(weights::DataFrame, dktnames)
+    _weights = dropmissing(weights)
+    w = Vector{Vector{Float64}}()
+    for (i, name) in enumerate(dktnames)
+        _df = filter(x -> x.Column1 == name, _weights)
+        _w = [_df.Comp_0[1], _df.Comp_1[1]]
+        @assert _w[1] > _w[2]
+        push!(w, _w)
+    end
+    w
+end
+
+weights = get_dkt_weights(dktweights, dktnames)
+
+ubase, upath = get_dkt_moments(gmm_moments, dktnames)
+mm = [MixtureModel([u0, ui], w) for (u0, ui, w) in zip(ubase, upath, weights)]
+u0 = mean.(ubase)
+cc = quantile.(mm, .99)
+
+# u0 = mean.(norm)
+# cc = quantile.(path, .99)
 
 describe(u0)
 std(u0)
@@ -59,4 +82,4 @@ begin
     f
 end
 
-save(projectdir("visualisation/models/output/carrying-capacities.jpeg"), f)
+save(projectdir("visualisation/models/output/carrying-capacities-mm.jpeg"), f)
