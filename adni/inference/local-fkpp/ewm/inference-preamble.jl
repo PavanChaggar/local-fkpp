6310f25e81c7aa60312e242829b5646d538e42fe
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2024-06-24 14:18:37 +0100

    refactoring scripts

--- adni/inference/local-fkpp/ewm/inference-preamble.jl
+++ adni/inference/local-fkpp/ewm/inference-preamble.jl
@@ -14,53 +14,23 @@ mtl = findall(x -> x ∈ mtl_regions, get_label.(cortex))
 neo_regions = ["inferiortemporal", "middletemporal"]
 neo = findall(x -> x ∈ neo_regions, get_label.(cortex))
 #-------------------------------------------------------------------------------
-# Data 
-#-----------------------------------------------------------------------------
-# sub_data_path = projectdir("adni/data/new_new_data/UCBERKELEY_TAU_6MM_18Dec2023_AB_STATUS.csv")
-# alldf = CSV.read(sub_data_path, DataFrame)
-# suvrnames = [ADNIDatasets.suvr_name.(dktnames); "INFERIORCEREBELLUM_SUVR"] # no pvc
-# d = Array(dropmissing(alldf[:, suvrnames]))
-# writedlm(projectdir("data-nopvc-ic.txt"), transpose(d ./ d[:,end]))
+# White matter reference 
+#-------------------------------------------------------------------------------
 sub_data_path = projectdir("adni/data/new_new_data/UCBERKELEY_TAU_6MM_18Dec2023_AB_STATUS.csv")
-alldf_nopvc = CSV.read(sub_data_path, DataFrame)
-
-sub_data_path_pvc = projectdir("adni/data/new_new_data/pvc/UC-Berkeley-TAUPVC-6MM-Mar-30-2024-AB-Status.csv")
-alldf = CSV.read(sub_data_path_pvc, DataFrame)
-
-function check_qc(df, rid)
-    _df = filter(x -> x.RID == rid, df)
-    if length(_df.qc_flag) == 0
-        return -1
-    end
-
-    if _df.qc_flag[1] == 2 && allequal(_df.qc_flag)
-        return 2
-    else
-        return -1
-    end
-end
-check_qc(rid) =  check_qc(alldf_nopvc, rid)
-
-alldf.qc_flag = map(check_qc, alldf.RID)
-# suvrnames = ADNIDatasets.suvr_name.(dktnames) # pvc
-# d = Array(dropmissing(alldf[:, suvrnames]))
-# writedlm(projectdir("py-analysis/data-pvc-ic.txt"), transpose(d))
+alldf = CSV.read(sub_data_path, DataFrame)
 
 #posdf = filter(x -> x.STATUS == "POS", alldf)
-
 posdf = filter(x -> x.AB_Status == 1, alldf)
 negdf = filter(x -> x.AB_Status == 0, alldf)
 
 dktdict = Connectomes.node2FS()
 dktnames = [dktdict[i] for i in get_node_id.(cortex)]
 
-data = ADNIDataset(posdf, dktnames; min_scans=3, reference_region="INFERIORCEREBELLUM", qc=true)
+data = ADNIDataset(posdf, dktnames; min_scans=3, reference_region="ERODED_SUBCORTICALWM")
 
 n_data = length(data)
 
-gmm_moments = CSV.read(projectdir("py-analysis/ic-pvc-moments-prob.csv"), DataFrame)
-# gmm_moments = CSV.read(projectdir("py-analysis/wm-nopvc-moments-prob.csv"), DataFrame)
-# gmm_moments = CSV.read(projectdir("adni/data/component_moments.csv"), DataFrame)
+gmm_moments = CSV.read(projectdir("py-analysis/wm-nopvc-moments-prob.csv"), DataFrame)
 
 mtl_cutoff = mean(gmm_moments.cutoff[mtl])
 neo_cutoff = mean(gmm_moments.cutoff[neo])
---
+++ adni/inference/local-fkpp/pvc/tauneg.jl
@@ -0,0 +1,136 @@
+using Pkg
+cd("/home/chaggar/Projects/local-fkpp")
+Pkg.activate(".")
+
+using Connectomes
+using ADNIDatasets
+using CSV, DataFrames
+using DrWatson: projectdir
+using DifferentialEquations
+using SciMLSensitivity
+using Zygote
+using Turing
+using Distributions
+using Serialization
+using DelimitedFiles, LinearAlgebra
+using Random
+using LinearAlgebra
+using SparseArrays
+include(projectdir("functions.jl"))
+  
+#-------------------------------------------------------------------------------
+# Load connectome, regional parameters and sort data
+#-------------------------------------------------------------------------------
+include(projectdir("adni/inference/local-fkpp/pvc/inference-preamble-pvc.jl"))
+
+subsuvr = calc_suvr.(neg_data)
+_subdata = [normalise(sd, u0, cc) for sd in subsuvr]
+
+blsd = [sd .- u0 for sd in _subdata]
+nonzerosubs = findall(x -> sum(x) < 2, [sum(sd, dims=1) .== 0 for sd in blsd])
+
+subdata = _subdata[nonzerosubs]
+vecsubdata = reduce(vcat, reduce(hcat, subdata))
+
+initial_conditions = [sd[:,1] for sd in subdata]
+times =  get_times.(neg_data[nonzerosubs])
+
+n_neg = length(neg_data[nonzerosubs])
+
+#-------------------------------------------------------------------------------
+# Connectome + ODE
+#-------------------------------------------------------------------------------
+L = laplacian_matrix(c)
+
+vols = [get_vol(neg_data, i) for i in nonzerosubs]
+init_vols = [v[:,1] for v in vols]
+max_norm_vols = reduce(hcat, [v ./ maximum(v) for v in init_vols])
+mean_norm_vols = vec(mean(max_norm_vols, dims=2))
+Lv = sparse(inv(diagm(mean_norm_vols)) * L)
+
+function NetworkLocalFKPP(du, u, p, t; L = Lv, u0 = u0, cc = cc)
+    du .= -p[1] * L * (u .- u0) .+ p[2] .* (u .- u0) .* ((cc .- u0) .- (u .- u0))
+end
+
+function make_prob_func(initial_conditions, p, a, times)
+    function prob_func(prob,i,repeat)
+        remake(prob, u0=initial_conditions[i], p=[p[i], a[i]], saveat=times[i])
+    end
+end
+
+function output_func(sol,i)
+    (sol,false)
+end
+
+
+prob = ODEProblem(NetworkLocalFKPP, 
+                  initial_conditions[1], 
+                  (0.,maximum(reduce(vcat, times))), 
+                  [1.0,1.0])
+                  
+sol = solve(prob, Tsit5())
+
+ensemble_prob = EnsembleProblem(prob, prob_func=make_prob_func(initial_conditions, ones(n_neg), ones(n_neg), times), output_func=output_func)
+ensemble_sol = solve(ensemble_prob, Tsit5(), trajectories=n_neg)
+
+function get_retcodes(es)
+    [SciMLBase.successful_retcode(sol) for sol in es]
+end
+
+function vec_sol(es)
+    reduce(vcat, [vec(sol) for sol in es])
+end
+
+#-------------------------------------------------------------------------------
+# Inference 
+#-------------------------------------------------------------------------------
+@model function localfkpp(data, prob, initial_conditions, times, n)
+    σ ~ LogNormal(0.0, 1.0)
+    
+    Pm ~ LogNormal(0.0,1.0)
+    Ps ~ LogNormal(0.0, 1.0)
+
+    Am ~ Normal(0.0, 1.0) # Normal(0.0,1.0)
+    As ~ LogNormal(0.0, 1.0)
+
+    ρ ~ filldist(truncated(Normal(Pm, Ps), lower=0), n)
+    α ~ filldist(Normal(Am, As), n)
+
+    ensemble_prob = EnsembleProblem(prob, 
+                                    prob_func=make_prob_func(initial_conditions, ρ, α, times), 
+                                    output_func=output_func)
+
+    ensemble_sol = solve(ensemble_prob, 
+                         Tsit5(), 
+                         abstol = 1e-9, 
+                         reltol = 1e-9, 
+                         trajectories=n, 
+                         sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))
+    if !allequal(get_retcodes(ensemble_sol)) 
+        Turing.@addlogprob! -Inf
+        println(findall(x -> x == 0, get_retcodes(ensemble_sol)))
+        println("failed")
+        return nothing
+    end
+    vecsol = vec_sol(ensemble_sol)
+
+    data ~ MvNormal(vecsol, σ^2 * I)
+end
+
+# Turing.setadbackend(:zygote)
+Random.seed!(1234); 
+
+m = localfkpp(vecsubdata, prob, initial_conditions, times, n_neg)
+m();
+
+println("starting inference")
+n_chains = 1
+n_samples = 2000
+pst = sample(m, 
+             Turing.NUTS(0.8), #, metricT=AdvancedHMC.DenseEuclideanMetric), 
+             n_samples)
+serialize(projectdir("adni/chains-revisions/local-fkpp/pvc-ic/pst-tauneg-$(n_chains)x$(n_samples).jls"), pst)
+
+#calc log likelihood 
+log_likelihood = pointwise_loglikelihoods(m, MCMCChains.get_sections(pst, :parameters));
+serialize(projectdir("adni/chains-revisions/local-fkpp/pvc-ic/ll-tauneg-$(n_chains)x$(n_samples).jls"), log_likelihood)
