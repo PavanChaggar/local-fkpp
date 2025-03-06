b35d12a5893a496f83803b3248768eece863fce3
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2025-03-06 10:42:01 +0100

    rm reversediff add logging

--- adni/inference/local-fkpp/abneg.jl
+++ adni/inference/local-fkpp/abneg.jl
@@ -113,52 +113,52 @@ end
     #                                 prob_func=make_prob_func(initial_conditions, ρ, α, times), 
     #                                 output_func=output_func)
 
-    # ensemble_sol = solve(ensemble_prob, 
-    #                      Tsit5(),
-    #                      abstol = 1e-9, 
-    #                      reltol = 1e-9, 
-    #                      trajectories=n, 
-    #                      sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))
-    # if !allequal(get_retcodes(ensemble_sol)) 
-    #     Turing.@addlogprob! -Inf
-    #     println("failed")
-    #     return nothing
-    # end
-    # vecsol = vec_sol(ensemble_sol)
-
-    # data ~ MvNormal(vecsol, σ^2 * I)
-    for i in 1:n
-        prob_n = remake(prob, u0 = initial_conditions[i], p = [ρ[i], α[i]])
-        # solve ode at time points specific to each subject
-        predicted = solve(
-            prob_n,
-            Tsit5(),
-            abstol=1e-6, 
-            reltol=1e-6,
-            saveat=times[i],
-            sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true))
-        )
-        if !SciMLBase.successful_retcode(sol) 
-            Turing.@addlogprob! -Inf
-            println("failed")
-            return nothing
-        end
-        Turing.@addlogprob! loglikelihood(MvNormal(vec(predicted), σ), data[i])
+    ensemble_sol = solve(ensemble_prob, 
+                         Tsit5(),
+                         abstol = 1e-6, 
+                         reltol = 1e-6, 
+                         trajectories=n, 
+                         sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)))
+    if !allequal(get_retcodes(ensemble_sol)) 
+        Turing.@addlogprob! -Inf
+        println("failed")
+        return nothing
     end
+    vecsol = vec_sol(ensemble_sol)
+
+    data ~ MvNormal(vecsol, σ^2 * I)
+    # for i in 1:n
+    #     prob_n = remake(prob, u0 = initial_conditions[i], p = [ρ[i], α[i]])
+    #     # solve ode at time points specific to each subject
+    #     predicted = solve(
+    #         prob_n,
+    #         Tsit5(),
+    #         abstol=1e-6, 
+    #         reltol=1e-6,
+    #         saveat=times[i],
+    #         sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true))
+    #     )
+    #     if !SciMLBase.successful_retcode(sol) 
+    #         Turing.@addlogprob! -Inf
+    #         println("failed")
+    #         return nothing
+    #     end
+    #     Turing.@addlogprob! loglikelihood(MvNormal(vec(predicted), σ), data[i])
+    # end
 end
 
 # Turing.setadbackend(:zygote)
 Random.seed!(1234); 
 
-m = localfkpp(vec.(subdata), prob, initial_conditions, times, n_subjects)
+m = localfkpp(vecsubdata, prob, initial_conditions, times, n_subjects)
 m();
 
 n_chains = 1
 n_samples = 2_000
 println("starting inference")
 pst = sample(m, 
-             Turing.NUTS(0.8, adtype=AutoZygote()),
+             Turing.NUTS(0.8), #, adtype=AutoZygote()),
              MCMCSerial(), 
              n_samples, 
-             n_chains, progress=false)
+             n_chains)
 serialize(projectdir("adni/new-chains/local-fkpp/length-free/pst-abneg-$(n_chains)x$(n_samples)-truncated-normal.jls"), pst)
